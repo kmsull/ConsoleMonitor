20bb37ed3dfd0d515b914fbcd3112da772b8800b
Kenneth <kmsullivanjr@uri.edu>
2023-06-28 14:20:27 -0400

    Start and Stop buttons edited to have 'active' and 'inactive' states

--- mainWindow.py
+++ mainWindow.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 
-def button1_clicked():
-    print("Button 1 clicked!")
+def start_button_clicked():
+    print("Start button clicked!")
+    start_button.config(state=tk.DISABLED, bg="gray")
+    stop_button.config(state=tk.NORMAL, bg="red")
 
-def button2_clicked():
-    print("Button 2 clicked!")
+def stop_button_clicked():
+    print("Stop button clicked!")
+    stop_button.config(state=tk.DISABLED, bg="gray")
+    start_button.config(state=tk.NORMAL, bg="green")
 
 root = tk.Tk()
 root.title("Resizable Window with Buttons")
 
 # Set the initial window size
-root.geometry("800x400")
+root.geometry("1600x800")
 
 # Configure the root window to be resizable in both directions
 root.resizable(True, True)
@@ -18,12 +22,12 @@ root.resizable(True, True)
 # Set the background color to white
 root.configure(bg="white")
 
-# Create the red button
-button1 = tk.Button(root, text="Button 1", bg="red", fg="white", relief=tk.RAISED, bd=2, command=button1_clicked)
-button1.place(relx=0.5, rely=0.9, anchor=tk.CENTER, relwidth=0.8, relheight=0.1)
+# Create the Start button
+start_button = tk.Button(root, text="Start", bg="green", fg="white", relief=tk.RAISED, bd=2, command=start_button_clicked)
+start_button.place(relx=0.5, rely=1.0, anchor=tk.S, relwidth=0.8, height=40, y=-70)
 
-# Create the green button above the red button
-button2 = tk.Button(root, text="Button 2", bg="green", fg="white", relief=tk.RAISED, bd=2, command=button2_clicked)
-button2.place(relx=0.5, rely=0.8, anchor=tk.CENTER, relwidth=0.8, relheight=0.1)
+# Create the Stop button
+stop_button = tk.Button(root, text="Stop", bg="gray", fg="white", relief=tk.RAISED, bd=2, state=tk.DISABLED, command=stop_button_clicked)
+stop_button.place(relx=0.5, rely=1.0, anchor=tk.S, relwidth=0.8, height=40, y=-20)
 
 root.mainloop()
